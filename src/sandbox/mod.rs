2d89f0092e1af4328b5bd260064d39500bcb92e0
akagiyuu <khanghuynh0245@gmail.com>
2025-12-21 16:04:06 +0700

    feat: init sandbox

--- src/sandbox/mod.rs
+++ src/sandbox/mod.rs
@@ -1,1 +1,20 @@
 mod resource;
+
+use anyhow::Result;
+use cgroups_rs::{CgroupPid, fs::Cgroup};
+pub use resource::Resource;
+
+use crate::Metrics;
+
+pub struct Sandbox {
+    pub cgroup: Cgroup,
+    pub resource: Resource,
+}
+
+impl Sandbox {
+    pub fn new(resource: Resource) -> Result<Sandbox> {
+        let cgroup: Cgroup = resource.try_into()?;
+
+        Ok(Sandbox { cgroup, resource })
+    }
+}
