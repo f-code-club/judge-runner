5d1d8512d9dbdf92e97e3299de54ab4e745871ab
akagiyuu <khanghuynh0245@gmail.com>
2025-12-21 16:23:09 +0700

    feat(sandbox): add wall time limit

--- src/sandbox/mod.rs
+++ src/sandbox/mod.rs
@@ -10,14 +10,22 @@ use crate::Metrics;
 
 pub struct Sandbox {
     pub cgroup: Cgroup,
-    pub time_limit: Duration,
+    pub cpu_time_limit: Duration,
+    pub wall_time_limit: Duration,
 }
 
 impl Sandbox {
     pub fn new(resource: Resource, time_limit: Duration) -> Result<Sandbox> {
         let cgroup: Cgroup = resource.try_into()?;
+        let cpu_time_limit = time_limit;
+        // TODO: need real usage to decide
+        let wall_time_limit = Duration::max(time_limit * 2, time_limit + Duration::from_secs(2));
 
-        Ok(Sandbox { cgroup, time_limit })
+        Ok(Sandbox {
+            cgroup,
+            cpu_time_limit,
+            wall_time_limit,
+        })
     }
 
     pub fn run(&self, child: Child) -> Result<Metrics> {
