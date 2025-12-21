356a0d30091c33d6e0730355da4378937ba4cbfe
akagiyuu <khanghuynh0245@gmail.com>
2025-12-21 16:01:27 +0700

    refactor: config all resource in struct

--- src/sandbox/resource.rs
+++ src/sandbox/resource.rs
@@ -1,27 +1,43 @@
 use std::time::Duration;
 
+use bon::Builder;
 use byte_unit::Byte;
-use cgroups_rs::fs::cgroup_builder::CgroupBuilder;
+use cgroups_rs::fs::{Cgroup, cgroup_builder::CgroupBuilder, hierarchies};
+use uuid::Uuid;
 
-pub enum Resource {
-    Memory(Byte),
-    Cpu { quota: Duration, period: Duration },
+const PREFIX: &str = "judge";
+
+#[derive(Clone, Copy, Builder)]
+pub struct Resource {
+    #[builder(default = Byte::GIGABYTE)]
+    pub memory: Byte,
+
+    #[builder(default = Duration::from_secs(1))]
+    pub cpu_quota: Duration,
+
+    #[builder(default = Duration::from_secs(1))]
+    pub cpu_period: Duration,
 }
 
-impl Resource {
-    pub fn add(self, builder: CgroupBuilder) -> CgroupBuilder {
-        match self {
-            Resource::Memory(byte) => builder
-                .memory()
-                .memory_swap_limit(0)
-                .memory_soft_limit(byte.as_u64() as i64)
-                .memory_hard_limit(byte.as_u64() as i64)
-                .done(),
-            Resource::Cpu { quota, period } => builder
-                .cpu()
-                .quota(quota.as_micros() as i64)
-                .period(period.as_micros() as u64)
-                .done(),
-        }
+impl TryFrom<Resource> for Cgroup {
+    type Error = anyhow::Error;
+
+    fn try_from(resource: Resource) -> Result<Self, Self::Error> {
+        let builder = CgroupBuilder::new(&format!("{}/{}", PREFIX, Uuid::new_v4()));
+
+        let memory = resource.memory.as_u64() as i64;
+        let builder = builder
+            .memory()
+            .memory_swap_limit(0)
+            .memory_soft_limit(memory)
+            .memory_hard_limit(memory)
+            .done();
+
+        let quota = resource.cpu_quota.as_micros() as i64;
+        let period = resource.cpu_period.as_micros() as u64;
+        let builder = builder.cpu().quota(quota).period(period).done();
+
+        let cgroup = builder.build(hierarchies::auto())?;
+        Ok(cgroup)
     }
 }
