b88135be8346674f18afdb8e36fe00c3c5d382c3
akagiyuu <khanghuynh0245@gmail.com>
2025-12-21 16:14:22 +0700

    feat: add time limit to sandbox

--- src/sandbox/mod.rs
+++ src/sandbox/mod.rs
@@ -1,5 +1,7 @@
 mod resource;
 
+use std::{process::Child, time::Duration};
+
 use anyhow::Result;
 use cgroups_rs::{CgroupPid, fs::Cgroup};
 pub use resource::Resource;
@@ -8,13 +10,20 @@ use crate::Metrics;
 
 pub struct Sandbox {
     pub cgroup: Cgroup,
-    pub resource: Resource,
+    pub time_limit: Duration,
 }
 
 impl Sandbox {
-    pub fn new(resource: Resource) -> Result<Sandbox> {
+    pub fn new(resource: Resource, time_limit: Duration) -> Result<Sandbox> {
         let cgroup: Cgroup = resource.try_into()?;
 
-        Ok(Sandbox { cgroup, resource })
+        Ok(Sandbox { cgroup, time_limit })
+    }
+
+    pub fn run(&self, child: Child) -> Result<Metrics> {
+        let pid = CgroupPid::from(child.id() as u64);
+        self.cgroup.add_task_by_tgid(pid)?;
+
+        todo!()
     }
 }
