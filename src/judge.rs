33ed571891da7c972910a6b0555e241ce2bf2147
akagiyuu <khanghuynh0245@gmail.com>
2025-12-25 03:54:53 +0700

    feat(judge): spawn checker and user submission and wire input and output

--- src/judge.rs
+++ src/judge.rs
@@ -1,12 +1,15 @@
 use std::{env, fs, io, path::PathBuf, time::Duration};
 
+use nix::{sys::stat, unistd};
 use state_shift::{impl_state, type_state};
 use uuid::Uuid;
 
 use crate::{Language, Resource, Sandbox, Verdict};
 
-const MAIN: &str = "main";
+const SUBMISSION: &str = "main";
 const CHECKER: &str = "checker";
+const INPUT: &str = "input";
+const OUTPUT: &str = "output";
 
 #[type_state(
     states = (Created, Compiled),
@@ -14,35 +17,25 @@ const CHECKER: &str = "checker";
 )]
 pub struct Judge {
     pub project_path: PathBuf,
-    pub sandbox: Sandbox,
     pub language: Language,
 }
 
 #[impl_state]
 impl Judge {
     #[require(Created)]
-    pub fn new(
-        code: &[u8],
-        checker: &[u8],
-        resource: Resource,
-        time_limit: Duration,
-        language: Language,
-    ) -> io::Result<Judge> {
+    pub fn new(code: &[u8], checker: &[u8], language: Language) -> io::Result<Judge> {
         let project_path = env::temp_dir().join(Uuid::new_v4().to_string());
         fs::create_dir(&project_path)?;
         let main_path = project_path
-            .with_file_name(MAIN)
+            .with_file_name(SUBMISSION)
             .with_extension(language.extension);
         let checker_path = project_path.with_file_name(CHECKER);
 
         fs::write(&main_path, code)?;
         fs::write(&checker_path, checker)?;
 
-        let sandbox = Sandbox::new(resource, time_limit)?;
-
         Ok(Judge {
             project_path,
-            sandbox,
             language,
         })
     }
@@ -50,7 +43,7 @@ impl Judge {
     #[require(Created)]
     #[switch_to(Compiled)]
     pub fn compile(&self) -> io::Result<Option<Verdict>> {
-        let Some(mut command) = self.language.get_compile_command(MAIN) else {
+        let Some(mut command) = self.language.get_compile_command(SUBMISSION) else {
             return Ok(None);
         };
         let mut process = command.current_dir(&self.project_path).spawn()?;
@@ -63,7 +56,54 @@ impl Judge {
     }
 
     #[require(Compiled)]
-    pub fn run(&self) -> io::Result<Verdict> {
-        todo!()
+    pub fn run(
+        self,
+        input: &[u8],
+        is_interactive: bool,
+        resource: Resource,
+        time_limit: Duration,
+    ) -> io::Result<Verdict> {
+        let checker_to_submission = self.project_path.join(INPUT);
+        unistd::mkfifo(&checker_to_submission, stat::Mode::S_IRWXU)?;
+        if !is_interactive {
+            fs::write(&checker_to_submission, input)?;
+        }
+
+        let submission_to_checker = self.project_path.join(OUTPUT);
+        unistd::mkfifo(&submission_to_checker, stat::Mode::S_IRWXU)?;
+        fs::write(&submission_to_checker, input)?;
+
+        let sandbox = Sandbox::new(resource, time_limit)?;
+
+        self.language
+            .get_run_command(CHECKER)
+            .current_dir(&self.project_path)
+            .stdin(
+                fs::OpenOptions::new()
+                    .read(true)
+                    .open(&submission_to_checker)?,
+            )
+            .stdout(
+                fs::OpenOptions::new()
+                    .write(true)
+                    .open(&checker_to_submission)?,
+            )
+            .spawn()?;
+        let submission = self
+            .language
+            .get_run_command(SUBMISSION)
+            .current_dir(&self.project_path)
+            .stdin(
+                fs::OpenOptions::new()
+                    .read(true)
+                    .open(&checker_to_submission)?,
+            )
+            .stdout(
+                fs::OpenOptions::new()
+                    .write(true)
+                    .open(&submission_to_checker)?,
+            )
+            .spawn()?;
+        sandbox.monitor(submission)
     }
 }
