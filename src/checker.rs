278da0fa584d663e83da395fa353dcdf9f09c3ad
akagiyuu <khanghuynh0245@gmail.com>
2025-12-25 13:49:51 +0700

    fix(checker): use join instead of with file name

--- src/checker.rs
+++ src/checker.rs
@@ -12,9 +12,7 @@ pub fn compile(code: &[u8], language: Language) -> io::Result<Vec<u8>> {
     };
     let project_path = env::temp_dir().join(Uuid::new_v4().to_string());
     fs::create_dir(&project_path)?;
-    let main = project_path
-        .with_file_name(MAIN)
-        .with_extension(language.extension);
+    let main = project_path.join(MAIN).with_extension(language.extension);
     fs::write(&main, code)?;
 
     let mut process = command.current_dir(&project_path).spawn()?;
