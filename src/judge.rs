2c30bbf09904c34b99347052d5c060d1ff70ff10
akagiyuu <khanghuynh0245@gmail.com>
2025-12-25 13:49:38 +0700

    feat(judge): remove state shift

--- src/judge.rs
+++ src/judge.rs
@@ -1,7 +1,6 @@
 use std::{env, fs, io, path::PathBuf, time::Duration};
 
 use nix::{sys::stat, unistd};
-use state_shift::{impl_state, type_state};
 use uuid::Uuid;
 
 use crate::{Language, Resource, Sandbox, Verdict};
@@ -11,25 +10,19 @@ const CHECKER: &str = "checker";
 const INPUT: &str = "input";
 const OUTPUT: &str = "output";
 
-#[type_state(
-    states = (Created, Compiled),
-    slots = (Created)
-)]
 pub struct Judge {
     pub project_path: PathBuf,
     pub language: Language,
 }
 
-#[impl_state]
 impl Judge {
-    #[require(Created)]
     pub fn new(code: &[u8], checker: &[u8], language: Language) -> io::Result<Judge> {
         let project_path = env::temp_dir().join(Uuid::new_v4().to_string());
         fs::create_dir(&project_path)?;
         let main_path = project_path
-            .with_file_name(SUBMISSION)
+            .join(SUBMISSION)
             .with_extension(language.extension);
-        let checker_path = project_path.with_file_name(CHECKER);
+        let checker_path = project_path.join(CHECKER);
 
         fs::write(&main_path, code)?;
         fs::write(&checker_path, checker)?;
@@ -40,9 +33,7 @@ impl Judge {
         })
     }
 
-    #[require(Created)]
-    #[switch_to(Compiled)]
-    pub fn compile(&self) -> io::Result<Option<Verdict>> {
+    pub fn compile(self) -> io::Result<Option<Verdict>> {
         let Some(mut command) = self.language.get_compile_command(SUBMISSION) else {
             return Ok(None);
         };
@@ -55,7 +46,6 @@ impl Judge {
         Ok(None)
     }
 
-    #[require(Compiled)]
     pub fn run(
         self,
         input: &[u8],
