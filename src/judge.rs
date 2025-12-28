262def2dad7a360e212109b145d9979c26e5bea1
akagiyuu <khanghuynh0245@gmail.com>
2025-12-28 14:42:11 +0700

    feat(judge): use tokio join and tokio select

--- src/judge.rs
+++ src/judge.rs
@@ -1,17 +1,13 @@
-use std::{
-    env, fs,
-    io::{self, Read, Write},
-    marker::PhantomData,
-    os::{fd::AsFd, unix::fs::OpenOptionsExt},
-    path::PathBuf,
-    process::Stdio,
-    thread,
-    time::Duration,
-};
+use std::{env, io, marker::PhantomData, path::PathBuf, process::Stdio, time::Duration};
 
 use bon::bon;
+use byte_unit::Byte;
 use state_shift::{impl_state, type_state};
-use timeout_readwrite::{TimeoutReadExt, TimeoutReader};
+use tokio::{
+    fs,
+    io::{AsyncReadExt, AsyncWriteExt},
+    time::sleep,
+};
 use uuid::Uuid;
 
 use crate::{Language, Metrics, Resource, Sandbox, Verdict};
@@ -19,7 +15,6 @@ use crate::{Language, Metrics, Resource, Sandbox, Verdict};
 const MAIN: &str = "main";
 const CHECKER: &str = "checker";
 const BUFFER_SIZE: usize = 512;
-const READ_TIMEOUT: Duration = Duration::from_millis(100);
 
 #[type_state(
     states = (Created, Compiled),
@@ -35,7 +30,7 @@ pub struct Judge {
 #[bon]
 impl Judge<Created> {
     #[builder]
-    pub fn new<'a>(
+    pub async fn new<'a>(
         #[rustfmt::skip]
         #[builder(with = |code: &'a [u8], language: Language| (code, language))]
         main: (&'a [u8], Language),
@@ -45,34 +40,40 @@ impl Judge<Created> {
         checker: Option<(&'a [u8], Language)>,
     ) -> io::Result<Judge<Created>> {
         let project_path = env::temp_dir().join(Uuid::new_v4().to_string());
-        fs::create_dir(&project_path)?;
+        fs::create_dir(&project_path).await?;
 
-        let (code, language) = main;
-        let main_path = project_path.join(MAIN).with_extension(language.extension);
-        fs::write(&main_path, code)?;
+        tokio::try_join! {
+            async {
+                let (code, language) = main;
+                let main_path = project_path.join(MAIN).with_extension(language.extension);
+                fs::write(&main_path, code).await?;
 
-        let checker_language = if let Some((code, language)) = checker {
-            let mut checker_path = project_path.join(CHECKER);
-            if language.is_interpreted() {
-                checker_path.set_extension(language.extension);
+                Ok::<_, io::Error>(())
+            },
+            async {
+                if let Some((code, language)) = checker {
+                    let mut checker_path = project_path.join(CHECKER);
+                    if language.is_interpreted() {
+                        checker_path.set_extension(language.extension);
+                    }
+                    let mut checker_file = fs::OpenOptions::new()
+                        .create(true)
+                        .write(true)
+                        .truncate(true)
+                        .mode(0o755)
+                        .open(&checker_path)
+                    .await?;
+                    checker_file.write_all(code).await?;
+                }
+
+                Ok(())
             }
-            let mut checker_file = fs::OpenOptions::new()
-                .create(true)
-                .write(true)
-                .truncate(true)
-                .mode(0o755)
-                .open(&checker_path)?;
-            checker_file.write_all(code)?;
-
-            Some(language)
-        } else {
-            None
-        };
+        }?;
 
         Ok(Judge {
             project_path,
-            language,
-            checker_language,
+            language: main.1,
+            checker_language: checker.map(|checker| checker.1),
             _state: PhantomData,
         })
     }
@@ -82,10 +83,10 @@ impl Judge<Created> {
 impl Judge {
     #[require(Created)]
     #[switch_to(Compiled)]
-    pub fn compile(self) -> io::Result<Result<Judge<Compiled>, Verdict>> {
+    pub async fn compile(self) -> io::Result<Result<Judge<Compiled>, Verdict>> {
         if let Some(mut cmd) = self.language.get_compile_command(MAIN) {
             let mut process = cmd.current_dir(&self.project_path).spawn()?;
-            let status = process.wait()?;
+            let status = process.wait().await?;
             if !status.success() {
                 return Ok(Err(Verdict::CompilationError));
             }
@@ -99,17 +100,17 @@ impl Judge {
     }
 
     #[require(Compiled)]
-    pub fn read_executable(&self) -> io::Result<Vec<u8>> {
+    pub async fn read_executable(&self) -> io::Result<Vec<u8>> {
         let mut path = self.project_path.join(MAIN);
         if self.language.is_interpreted() {
             path.set_extension(self.language.extension);
         }
 
-        fs::read(path)
+        fs::read(path).await
     }
 
     #[require(Compiled)]
-    pub fn run(
+    pub async fn run(
         self,
         input: &[u8],
         is_interactive: bool,
@@ -132,7 +133,7 @@ impl Judge {
             .stderr(Stdio::null())
             .spawn()?;
         let mut cstdin = checker.stdin.take().unwrap();
-        let cstdout = checker.stdout.take().unwrap().with_timeout(READ_TIMEOUT);
+        let mut cstdout = checker.stdout.take().unwrap();
 
         let sandbox = Sandbox::new(resource, time_limit)?;
         let mut cmd = language.get_run_command(MAIN);
@@ -142,48 +143,103 @@ impl Judge {
             .stderr(Stdio::piped());
         let mut main = sandbox.spawn(cmd)?;
         let mut stdin = main.stdin.take().unwrap();
-        let stdout = main.stdout.take().unwrap().with_timeout(READ_TIMEOUT);
-        let mut stderr = main.stderr.take().unwrap().with_timeout(READ_TIMEOUT);
+        let mut stdout = main.stdout.take().unwrap();
+        let mut stderr = main.stderr.take().unwrap();
 
-        let ((verdict, run_time, memory_usage), stdout) = thread::scope(|scope| {
-            let monitor_thread = scope.spawn(|| sandbox.monitor(main));
+        let monitor = tokio::spawn(async move { sandbox.monitor(main).await });
 
-            if !is_interactive {
-                stdin.write_all(input)?;
-                stdin.write_all(b"\n")?;
-                stdin.flush()?;
+        tokio::try_join! {
+            async {
+                if !is_interactive {
+                    stdin.write_all(input).await?;
+                    stdin.write_all(b"\n").await?;
+                    stdin.flush().await?;
+                }
+
+                Ok::<_, io::Error>(())
+            },
+            async {
+                cstdin.write_all(input).await?;
+                cstdin.write_all(b"\n").await?;
+                cstdin.flush().await?;
+
+                Ok::<_, io::Error>(())
             }
-            cstdin.write_all(input)?;
-            cstdin.write_all(b"\n")?;
-            cstdin.flush()?;
+        }?;
 
-            scope.spawn(|| forward(cstdout, stdin));
-            let main_to_checker = scope.spawn(|| forward(stdout, cstdin));
+        let mut out: Vec<u8> = vec![];
+        let mut err: Vec<u8> = vec![];
+        let mut verdict: Option<Verdict> = None;
+        let mut run_time: Duration = Duration::default();
+        let mut memory_usage: Byte = Byte::default();
+        tokio::select! {
+            monitor_result = monitor => {
+                let monitor_result = monitor_result.unwrap()?;
+                (verdict, run_time, memory_usage) = monitor_result;
+            }
+            err = async {
+                let mut buffer = [0u8; BUFFER_SIZE];
+                loop {
+                    let n = stdout.read(&mut buffer).await?;
+                    if n == 0 {
+                        break;
+                    }
+                    if cstdin.write_all(&buffer[..n]).await.is_err() {
+                        break;
+                    }
+                    cstdin.flush().await?;
+                    out.extend_from_slice(&buffer[0..n]);
+                }
 
-            let monitor_result = match monitor_thread.join().unwrap() {
-                Ok(v) => v,
-                Err(err) => return Err(err),
-            };
-            let output = main_to_checker.join().unwrap()?;
-            let output = String::from_utf8(output).map_err(io::Error::other)?;
+                // sleep indefinitely until sandbox return
+                sleep(Duration::MAX).await;
 
-            Ok((monitor_result, output))
-        })?;
-        let mut err = String::new();
-        // TODO: handle error
-        let _ = stderr.read_to_string(&mut err);
+                Ok::<_, io::Error>(())
+            } => { err? }
+            err = async {
+                let mut buffer = [0u8; BUFFER_SIZE];
+                loop {
+                    let n = cstdout.read(&mut buffer).await?;
+                    if n == 0 { break; }
+                    if stdin.write_all(&buffer[..n]).await.is_err() {
+                        break;
+                    }
+                    stdin.flush().await?;
+                }
+
+                // sleep indefinitely until sandbox return
+                sleep(Duration::MAX).await;
+
+                Ok::<_, io::Error>(())
+            } => { err? }
+            err = async {
+                let mut buffer = [0u8; BUFFER_SIZE];
+                loop {
+                    let n = stderr.read(&mut buffer).await?;
+                    if n == 0 { break; }
+                    err.extend_from_slice(&buffer[0..n]);
+                }
+
+                // sleep indefinitely until sandbox return
+                sleep(Duration::MAX).await;
+
+                Ok::<_, io::Error>(())
+            } => { err? }
+        };
+        let out = String::from_utf8(out).map_err(io::Error::other)?;
+        let err = String::from_utf8(err).map_err(io::Error::other)?;
 
         if let Some(verdict) = verdict {
             return Ok(Metrics {
                 verdict,
                 run_time,
-                stdout,
+                stdout: out,
                 stderr: err,
                 memory_usage,
             });
         }
 
-        let status = checker.wait()?;
+        let status = checker.wait().await?;
         let verdict = if status.success() {
             Verdict::Accepted
         } else {
@@ -193,32 +249,9 @@ impl Judge {
         Ok(Metrics {
             verdict,
             run_time,
-            stdout,
+            stdout: out,
             stderr: err,
             memory_usage,
         })
     }
-}
-
-fn forward<R: Read + AsFd, W: Write>(
-    mut reader: TimeoutReader<R>,
-    mut writer: W,
-) -> io::Result<Vec<u8>> {
-    let mut stdout: Vec<u8> = vec![];
-    let mut buffer = [0u8; BUFFER_SIZE];
-    loop {
-        // TODO:handle error
-        let n = match reader.read(&mut buffer) {
-            Ok(0) | Err(_) => break,
-            Ok(n) => n,
-        };
-        if writer.write_all(&buffer[..n]).is_err() {
-            break;
-        }
-        stdout.extend_from_slice(&buffer[0..n]);
-        writer.flush()?;
-    }
-    drop(writer);
-
-    Ok(stdout)
 }
